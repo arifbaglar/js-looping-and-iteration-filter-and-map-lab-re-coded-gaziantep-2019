e7a54eba3a9b681d0ea239a306b02e5fc4636c83
Arif BAĞLAR <arifbaglar@hotmail.com>
2019-07-07 01:12:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here:
+function driversWithRevenueOver(array,number){
+  const result = array.filter(arif => arif.revenue > number);
+  return result;
+}
+function driverNamesWithRevenueOver(array,number){
+  const result = array.filter(arif => arif.revenue > number);
+  const result2 =result.map(isim=>isim.name);
+  return result2;
+}
+
+function exactMatch(array,{key:value}){
+  const newarray=array.filter(arif=> arif.name==array.key);
+  return newarray;
+  
+}
